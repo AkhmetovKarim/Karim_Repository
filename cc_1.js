013968fb5926af0b49474ef9f1e2b4ee17c35720
Karim <konanfoi@gmail.com>
2025-01-25 19:33:04 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,8 @@
+//Task 1 - Employee Information
+
+let employeeName = "Cristiano Ronaldo";
+console.log(employeeName, typeof employeeName);
+const employeeID = 7777;
+console.log(employeeID, typeof employeeID);
+var isActive = true;
+console.log(isActive, typeof isActive);
