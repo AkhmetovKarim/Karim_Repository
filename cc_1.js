8ce1c3535136c6919b64b49358a8cfadeef61ac6
Karim <konanfoi@gmail.com>
2025-01-25 19:43:34 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -6,3 +6,12 @@ const employeeID = 7777;
 console.log(employeeID, typeof employeeID);
 var isActive = true;
 console.log(isActive, typeof isActive);
+
+//Task - 2 Product Details
+
+let productName = "Vodka Absolute";
+console.log(productName, typeof productName);
+const productPrice = 24.99;
+console.log(productPrice, typeof productPrice);
+var isAvailable = true;
+console.log(isAvailable, typeof isAvailable);
